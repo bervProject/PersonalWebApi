9a66f6edd941119ff82b139a569279ab0ee32969
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-10-09 18:51:42 +0700

    Add Another Test

---
+++ PersonalWebApi.Integration.Test/ExperiencesControllerTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using PersonalWebApi.Integration.Test.Models;
+using PersonalWebApi.Models;
+using System.Collections.Generic;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PersonalWebApi.Integration.Test
+{
+    public class ExperiencesControllerTest : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+        private readonly string _baseUrl = "/v1/Experiences";
+
+        public ExperiencesControllerTest(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task Get()
+        {
+            var client = _factory.CreateClient();
+            var response = await client.GetAsync(_baseUrl);
+            response.EnsureSuccessStatusCode();
+            var bodyResponse = await response.Content.ReadFromJsonAsync<ODataListResponse<Experience>>();
+            var emptyResult = new List<Experience>();
+            Assert.Equal(emptyResult, bodyResponse.Value);
+        }
+    }
+}
---
+++ PersonalWebApi.Integration.Test/ProjectsControllerTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using PersonalWebApi.Integration.Test.Models;
+using PersonalWebApi.Models;
+using System.Collections.Generic;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PersonalWebApi.Integration.Test
+{
+    public class ProjectsControllerTest : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+        private readonly string _baseUrl = "/v1/Projects";
+
+        public ProjectsControllerTest(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task Get()
+        {
+            var client = _factory.CreateClient();
+            var response = await client.GetAsync(_baseUrl);
+            response.EnsureSuccessStatusCode();
+            var bodyResponse = await response.Content.ReadFromJsonAsync<ODataListResponse<Project>>();
+            var emptyResult = new List<Project>();
+            Assert.Equal(emptyResult, bodyResponse.Value);
+        }
+    }
+}
